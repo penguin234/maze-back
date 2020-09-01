9e6cc43086e05b561c08b9408f52704d87239712
penguin <hjh6195@naver.com>
2020-09-01 17:17:40 +0900

    allowed host

--- maze/settings.py
+++ maze/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'w^7e!1hdc9^@bxx$3h127zftve26-p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['penguins-maze-backend.herokuapp.com']
 
 
 # Application definition
